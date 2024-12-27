c9a32848099c41e541f629f7afc469697b913396
tchex <tchex@users.noreply.github.com>
2024-12-27 15:11:53 +0100

    Update coordinates.dart

--- lib/src/model/coordinates.dart
+++ lib/src/model/coordinates.dart
@@ -7,11 +7,16 @@ class Coordinates {
   /// Longitude coordinate value.
   double? longitude;
 
+  /// Number of Matches found.
+  double? matches;
+
   Coordinates({this.latitude, this.longitude});
 
   factory Coordinates.fromJson(Map<String, dynamic> coordinates) => Coordinates(
       latitude: double.tryParse(tryParse(coordinates['latt']) ?? ''),
-      longitude: double.tryParse(tryParse(coordinates['longt']) ?? ''));
+      longitude: double.tryParse(tryParse(coordinates['longt']) ?? ''),
+      matches: double.tryParse(tryParse(coordinates['matches']) ?? ''),
+  );
 
   @override
   String toString() => "GEOCODE: longitude=$longitude, latitude=$latitude";
